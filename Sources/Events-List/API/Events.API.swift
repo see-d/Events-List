2239cbd36d61f2e48be7ad0bfb61084b90b3f132
Corey Duncan <see.duncan@gmail.com>
2023-06-20 10:27:41 +0300

    Add API and empty repository

---
+++ Sources/Events-List/API/Events.API.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Corey Duncan on 20/6/23.
+//
+
+import Foundation
+import API_Service
+
+extension Feature.API {
+    enum Sports {
+        case fetch
+    }
+}
+
+extension Feature.API.Sports : API {
+    var scheme: String {
+        return "https"
+    }
+    
+    var host: String {
+        return "618d3aa7fe09aa001744060a.mockapi.io"
+    }
+    
+    var path: String {
+        return "/api"
+    }
+    
+    var endpoint: String {
+        return "/sports"
+    }
+    
+    var query: [URLQueryItem] {
+        return []
+    }
+    
+    var method: String {
+        return "GET"
+    }
+}
---
+++ Sources/Events-List/Repository/Events.Repository.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Corey Duncan on 20/6/23.
+//
+
+import Foundation
+import API_Service
+
+struct EventsRepository {
+
+    func fetch() {
+    
+    }
+}
