a3584c4fb908998876db17699269c10223ca2fee
Corey Duncan <see.duncan@gmail.com>
2023-06-20 10:39:06 +0300

    Implement Fetch and Repository

--- Sources/Events-List/Repository/Events.Repository.swift
+++ Sources/Events-List/Repository/Events.Repository.swift
@@ -8,9 +8,20 @@
 import Foundation
 import API_Service
 
-struct EventsRepository {
-
-    func fetch() {
+class EventsRepository: Repository {
+    var data:[Feature.API.Sport] = []
     
+    func fetch() {
+        let api = Feature.API.Sports.fetch
+        API_Service.make(request: api) { [weak self] (response:[Feature.API.Sport]?, error:Error?) in
+            DispatchQueue.main.async {
+                guard let self else { return }
+                guard let response else {
+                    let error = error ?? NSError(domain: "fetch.repository.events", code: 400)
+                    return
+                }
+                self.data = response
+            }
+        }
     }
 }
