d3399c8a529d7a42c74aae59357e26f2526e3342
Corey Duncan <see.duncan@gmail.com>
2023-06-21 11:37:59 +0300

    Add secondary background color for card

--- Sources/Events-List/Shared/Palette.swift
+++ Sources/Events-List/Shared/Palette.swift
@@ -9,6 +9,7 @@ import UIKit
 
 enum Palette: String {
     case background = "background"
+    case secondaryBackground = "secondary.background"
     case darkText = "dark.text"
     case favorite = "favorite"
 }
@@ -18,6 +19,8 @@ extension Palette {
         switch self {
         case .background:
             return UIColor(named: "background", in: .module, compatibleWith: .none)!
+        case .secondaryBackground:
+            return UIColor(named: "secondary.background", in: .module, compatibleWith: .none)!
         case .darkText:
             return UIColor(named: "dark.text", in: .module, compatibleWith: .none)!
         case .favorite:
