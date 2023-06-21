6ecea79df5d6bf3f8ef3bc30b4041f3ba211e904
Corey Duncan <see.duncan@gmail.com>
2023-06-21 17:53:52 +0300

    change swift tools version -> 5.71

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/see-d/API-Service.git", from: "1.0.1"),
+        .package(url: "https://github.com/see-d/API-Service.git", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
